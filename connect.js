98b4f953d52c09c5d2f8edccc6a1c1e9e931c07e
RK <romainknezevic@gmail.com>
2017-03-14 10:53:14 +0100

    scroll to bottom if not enough elements in page

--- connect.js
+++ connect.js
@@ -1,5 +1,9 @@
 setInterval(function() {
   var connectBtns = document.getElementsByClassName("mn-person-card__person-btn-ext");
+  if connectBtns.length < 10 {
+    // scroll bottom to trigger ajax call filling the list
+    window.scrollTo(0, document.body.scrollHeight);
+  }
   for (var i = 0; i<connectBtns.length; i++) {
     console.log(connectBtns[i].parentNode.parentNode.getElementsByClassName("mn-person-info__name")[0].innerText);
     connectBtns[i].click();
