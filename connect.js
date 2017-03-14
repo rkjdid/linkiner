5e654c8741e7a57c8066446e46342c287be4de12
RK <romainknezevic@gmail.com>
2017-03-14 10:47:53 +0100

    connect spammer

---
+++ connect.js
@@ -0,0 +1,8 @@
+setInterval(function() {
+  var connectBtns = document.getElementsByClassName("mn-person-card__person-btn-ext");
+  for (var i = 0; i<connectBtns.length; i++) {
+    console.log(connectBtns[i].parentNode.parentNode.getElementsByClassName("mn-person-info__name")[0].innerText);
+    connectBtns[i].click();
+  }
+}, 1000);
+
